40a9efb82da27af9af84017d0b9f3f41a92b0204
DietherPano <dietherpano95@gmail.com>
2026-03-26 17:17:22 +0800

    added

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const dailyIcons = [
+  'https://res.cloudinary.com/dqslpwgdr/image/upload/v1774516415/PANO_DIETHER_DITA1034_clncva_idvf4c.ico',
+  'https://res.cloudinary.com/dqslpwgdr/image/upload/v1774516415/wp16139343-cosmic-princess-wallpapers_twh9ub.ico',
+]
+
+const setDailyFavicon = () => {
+  const daysSinceEpoch = Math.floor(Date.now() / 86400000)
+  const iconForToday = dailyIcons[daysSinceEpoch % dailyIcons.length]
+
+  let favicon = document.querySelector("link[rel='icon']")
+  if (!favicon) {
+    favicon = document.createElement('link')
+    favicon.setAttribute('rel', 'icon')
+    document.head.appendChild(favicon)
+  }
+
+  favicon.setAttribute('type', 'image/x-icon')
+  favicon.setAttribute('href', iconForToday)
+}
+
+setDailyFavicon()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
